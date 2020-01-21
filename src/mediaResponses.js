652ef70800e99f3199fbdbdc0f98fe1b7b3d9228
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2020-01-21 17:07:17 -0500

    Having issue with bird.mp4

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,47 +1,47 @@
 const fs = require('fs');
 const path = require('path');
 
-const getParty = (request, response) => {
-  const file = path.resolve(__dirname, '../client/party.mp4');
+// const getParty = (request, response) => {
+//   const file = path.resolve(__dirname, '../client/party.mp4');
 
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        response.writeHead(404);
-      }
-      response.end(err);
-    }
+//   fs.stat(file, (err, stats) => {
+//     if (err) {
+//       if (err.code === 'ENOENT') {
+//         response.writeHead(404);
+//       }
+//       response.end(err);
+//     }
 
-    let { range } = request.headers;
-    if (!range) {
-      range = 'bytes=0-';
-    }
-    const positions = range.replace(/bytes=/, '').split('-');
+//     let { range } = request.headers;
+//     if (!range) {
+//       range = 'bytes=0-';
+//     }
+//     const positions = range.replace(/bytes=/, '').split('-');
 
-    let start = parseInt(positions[0], 10);
+//     let start = parseInt(positions[0], 10);
 
-    const total = stats.size;
-    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+//     const total = stats.size;
+//     const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
-    if (start > end) {
-      start = end - 1;
-    }
+//     if (start > end) {
+//       start = end - 1;
+//     }
 
-    const chunkSize = (end - start) + 1;
+//     const chunkSize = (end - start) + 1;
 
-    response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/${total}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunkSize,
-      'Content-Type': 'video/mp4',
-    });
+//     response.writeHead(206, {
+//       'Content-Range': `bytes ${start}-${end}/${total}`,
+//       'Accept-Ranges': 'bytes',
+//       'Content-Length': chunkSize,
+//       'Content-Type': 'video/mp4',
+//     });
 
-    const stream = fs.createReadStream(file, { start, end });
-    stream.on('open', () => stream.pipe(response));
-    stream.on('error', (streamErr) => response.end(streamErr));
-    return stream;
-  });
-};
+//     const stream = fs.createReadStream(file, { start, end });
+//     stream.on('open', () => stream.pipe(response));
+//     stream.on('error', (streamErr) => response.end(streamErr));
+//     return stream;
+//   });
+// };
 
 const loadFile = (request, response, filePath, mediaType) => {
     const file = path.resolve(__dirname, filePath);
@@ -83,6 +83,7 @@ const loadFile = (request, response, filePath, mediaType) => {
       stream.on('error', (streamErr) => response.end(streamErr));
       return stream;
     });  
-}
+};
 
-module.exports.getParty = getParty;
+// module.exports.getParty = getParty;
+module.exports.loadFile = loadFile;
--- src/server.js
+++ src/server.js
@@ -10,14 +10,23 @@ const onRequest = (request, response) => {
         htmlHandler.getIndex(request, response);
         break;
     case '/party.mp4':
-        mediaHandler.getParty(request, response);
+        //mediaHandler.getParty(request, response);
+        mediaHandler.loadFile(request, response, "../client/party.mp4", "video/mp4");
         break;
     case '/page2':
         htmlHandler.getPage2(request, response);
-        break;        
+        break;
+    case '/bling.mp3':
+        mediaHandler.loadFile(request, response, "../client/bling.mp3", "audio/mpeg");
+        break;
+    case "/page3":
+        htmlHandler.getPage3(request, response);
+        break;
+    case "/bird.mp4":
+        mediaHandler.loadFile(request, response, "../client/bird.mp4", "video/mp4");
     default:
-      htmlHandler.getIndex(request, response);
-      break;
+        htmlHandler.getIndex(request, response);
+        break;
   }
 };
 
